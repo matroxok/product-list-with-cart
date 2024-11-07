46b07647081caecce427c837cc7fe6f71bc4df0a
Mateusz Kozera <mateusz.kozera@pwr.edu.pl>
2024-11-07 12:45:53 +0100

    new

---
+++ cart-app/components/product-box.jsx
@@ -0,0 +1,12 @@
+import AddCartButton from './addCartButton'
+
+const ProductBox = () => {
+	return (
+		<div className='w-full h-full bg-black'>
+			<AddCartButton></AddCartButton>
+			<h1 className='text-5xl'>test</h1>
+		</div>
+	)
+}
+
+export default ProductBox
---
+++ cart-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import ProductBox from '../components/product-box'
+
+function App() {
+	return (
+		<div className='p-8'>
+			<h1 className='text-4xl font-bold'>Desserts</h1>
+			<div className='flex items-center justify-center mt-5'>
+				<ProductBox></ProductBox>
+			</div>
+		</div>
+	)
+}
+
+export default App
